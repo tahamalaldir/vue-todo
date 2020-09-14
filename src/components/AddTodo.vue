1f6c64cb120ae61ad23a5090a507624b2fc9373c
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-09-14 22:30:49 +0300

    AddTodo Component Created

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-col>
+    <b-input-group class="w-75 mx-auto">
+      <b-form-input
+        class="mr-2"
+        size="lg"
+        v-model="todo.id"
+        @keyup.enter="handleAdd"
+      ></b-form-input>
+      <b-form-input
+        class="mr-2"
+        size="lg"
+        v-model="todo.text"
+        @keyup.enter="handleAdd"
+      ></b-form-input>
+      <b-input-group-append>
+        <b-button
+          class="color"
+          variant="warning"
+          :disabled="!todo.text"
+          @click="handleAdd"
+          >Add a Todo</b-button
+        >
+      </b-input-group-append>
+    </b-input-group>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      todo: { id: "", text: "" },
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.$emit("add:todo", this.todo);
+      this.todo.text = "";
+      this.todo.id = "";
+    },
+  },
+};
+</script>
+
+<style></style>
