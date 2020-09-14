56364dba12f67e4bc4710eb3e31a9dea711b000d
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-09-14 03:11:37 +0300

    Components Edited

--- src/components/ContinuesTodo.vue
+++ src/components/ContinuesTodo.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h1>Continues Todos</h1>
+    <ul v-if="!Todos.length">
+      Todo Yok
+    </ul>
+    <ul v-else>
+      <li v-for="todo in Todos" :key="todo">
+        {{ todo }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "ContinuesTodo",
+  props: {
+    Todos: Array,
+  },
 };
 </script>
 
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
     <h1>Todos</h1>
-    <ContinuesTodo />
+    <input type="text" v-model="todoText" @keyup.enter="addNewTodo" />
+    <button v-if="todoText.length > 0" @click="addNewTodo">Tıkla</button>
+
+    <ContinuesTodo :Todos="Todos" />
     <CompletedTodo />
   </div>
 </template>
 
 <script>
+//import { eventBus } from "../main";
 import ContinuesTodo from "./ContinuesTodo.vue";
 import CompletedTodo from "./CompletedTodo.vue";
 export default {
   name: "Todo",
   components: { ContinuesTodo, CompletedTodo },
+  data() {
+    return {
+      todoText: "",
+      Todos: [],
+    };
+  },
+  methods: {
+    addNewTodo: function() {
+      this.Todos.push(this.todoText);
+      this.todoText = "";
+    },
+  },
 };
 </script>
 
