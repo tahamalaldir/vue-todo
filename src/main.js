25554042e823c8a377b7baaea76dbae0b3e9349d
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-09-20 02:29:11 +0300

    UUID Added

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import UUID from "vue-uuid";
+
+Vue.use(UUID);
 // Install BootstrapVue
 Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
