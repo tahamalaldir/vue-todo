44da6ca265ab6171423fb2cdf6adc16cb26440cb
Taha Malaldır <tevfikhanmalaldir@gmail.com>
2020-12-21 01:09:02 +0300

    Vuex store added

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import UUID from "vue-uuid";
-
+import store from "./store";
 Vue.use(UUID);
 // Install BootstrapVue
 Vue.use(BootstrapVue);
@@ -14,5 +14,6 @@ Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   render: (h) => h(App),
 }).$mount("#app");
